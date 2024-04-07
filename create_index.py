57263048b03b5ae1cecdccf76321e023ca38d000
GregoryTomy <95327355+GregoryTomy@users.noreply.github.com>
2024-04-07 11:51:52 -0600

    update index script

--- create_index.py
+++ create_index.py
@@ -1,13 +1,26 @@
+#! Update Index
 import os
 
 from azure.core.credentials import AzureKeyCredential
 from azure.search.documents.indexes import SearchIndexClient
-from azure.search.documents.indexes.models import (SimpleField, 
-                                                   SearchIndex, 
-                                                   SearchFieldDataType)
+from azure.search.documents.indexes.models import (SimpleField,
+                                                   SearchIndex,
+                                                   SearchFieldDataType,
+                                                   SearchableField,
+                                                   SearchField,
+                                                   VectorSearch,
+                                                   HnswAlgorithmConfiguration,
+                                                   HnswParameters,
+                                                   VectorSearchAlgorithmKind,
+                                                   VectorSearchAlgorithmMetric,
+                                                   VectorSearchProfile)
 
 service_endpoint = os.getenv('SEARCH_SERVICE_NAME')
 service_key = os.getenv('SEARCH_API_KEY')
+
+
+print("Service Endpoint:", service_endpoint)
+print("Service API Key:", service_key)
 
 # instantiate client
 client = SearchIndexClient(service_endpoint,
@@ -25,23 +38,61 @@ client = SearchIndexClient(service_endpoint,
 
 def create_index():
     name= "wine-index"
+
+    # # Check if the index already exists
+    # existing_indexes = [index.name for index in client.list_indexes()]
+    # if name in existing_indexes:
+    #     # Delete the existing index
+    #     client.delete_index(name)
+    #     print(f"Existing index '{name}' deleted successfully.")
+
     fields = [
         SimpleField(name="id", type=SearchFieldDataType.String, key=True),
-        SimpleField(name="name", type=SearchFieldDataType.String, searchable=True),
-        SimpleField(name="grape", type=SearchFieldDataType.String, searchable=True),
-        SimpleField(name="region", type=SearchFieldDataType.String, searchable=True),
-        SimpleField(name="variety", type=SearchFieldDataType.String, searchable=True),
-        SimpleField(name="rating", type=SearchFieldDataType.Double, searchable=True),
-        SimpleField(name="notes", type=SearchFieldDataType.String, searchable=True),
+        SearchableField(name="name", type=SearchFieldDataType.String, searchable=True, retrievable=True),
+        SearchableField(name="grape", type=SearchFieldDataType.String, searchable=True, retrievable=True),
+        SearchableField(name="region", type=SearchFieldDataType.String, searchable=True, retrievable=True),
+        SearchableField(name="variety", type=SearchFieldDataType.String, searchable=True, retrievable=True),
+        SearchableField(name="rating", type=SearchFieldDataType.Double, searchable=True, retrievable=True),
+        SearchableField(name="notes", type=SearchFieldDataType.String, searchable=True, retrievable=True),
+        SearchableField(name="content", type=SearchFieldDataType.String, searchable=True, retrievable=True),
+        SearchableField(name="metadata", type=SearchFieldDataType.String, searchable=True, retrievable=True),
+        SearchField(name="content_vector",
+                    type=SearchFieldDataType.Collection(SearchFieldDataType.Single),
+                    vector_search_dimensions=1536,
+                    vector_search_profile_name="my-vector-config",
+                    )
     ]
+
+    # configure the vector search
+    vector_search = VectorSearch(
+        algorithms=[
+            HnswAlgorithmConfiguration(
+                name="my-hnsw",
+                kind=VectorSearchAlgorithmKind.HNSW,
+                parameters=HnswParameters(
+                    m=4,
+                    ef_construction=400,
+                    ef_search=500,
+                    metric=VectorSearchAlgorithmMetric.COSINE,
+                )
+            )
+        ],
+        profiles=[
+            VectorSearchProfile(
+                name="my-vector-config",
+                algorithm_configuration_name="my-hnsw"
+            )
+        ]
+    )
 
     # create the index
     index = SearchIndex(name=name,
-                        fields=fields)
+                        fields=fields,
+                        vector_search=vector_search)
     
     # create index
-    client.create_index(index)
-    print(f"Index '{name} created successfully")
+    result = client.create_index(index)
+    print(f"Index '{result.name} created successfully")
     
 if __name__ == "__main__":
     create_index()
--- embeddings.py
+++ embeddings.py
@@ -1,15 +1,14 @@
 import os
 import openai
 from dotenv import load_dotenv
-from langchain_community.embeddings.openai import OpenAIEmbeddings
-from langchain_community.embeddings.llamacpp import LlamaCppEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores.azuresearch import AzureSearch
 from langchain_community.document_loaders.csv_loader import CSVLoader
 from langchain_text_splitters import CharacterTextSplitter
 
 load_dotenv('.env')
 
-loader = CSVLoader("wine-ratings.csv")
+loader = CSVLoader("wine-ratings_100.csv")
 documents = loader.load()
 
 
@@ -20,21 +19,20 @@ split_docs =text_splitter.split_documents(documents)
 
 print(os.getenv('SEARCH_SERVICE_NAME'))
 
-#get embeddings
-# embeddings= OpenAIEmbeddings(deployment="wine-embedding",
-#                              chunk_size=1)
-
-embeddings_llama = LlamaCppEmbeddings(model_path=os.getenv('LLAMA_MODEL_PATH'))
+embeddings = OpenAIEmbeddings()
 
 # # LLama embeddings test
-# test_string = "This is a test document."
-# test_string_embedding = embeddings_llama.embed_query(test_string)
+# test_string = ["This is a test document.",
+#                "To check the dimensions.",
+#                "Should be 3x 4096"]
+# test_string_embedding = embeddings_llama.embed_documents(test_string)
 
+# print(len(test_string_embedding), len(test_string_embedding[0]), len(test_string_embedding[2]))
 
 # Connect and load to Azure Cognitive Search
 azure_cog_search = AzureSearch(azure_search_endpoint=os.getenv('SEARCH_SERVICE_NAME'),
                                azure_search_key=os.getenv('SEARCH_API_KEY'),
                                index_name=os.getenv('SEARCH_INDEX_NAME'),
-                               embedding_function=embeddings_llama.embed_query)
+                               embedding_function=embeddings.embed_query)
 
-azure_cog_search.add_documents(documents=split_docs)
+# azure_cog_search.add_documents(documents=split_docs)
