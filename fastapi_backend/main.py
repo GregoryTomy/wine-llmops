c484096c2074e3c18cd4f4765b47ffc405a6fa7b
GregoryTomy <95327355+GregoryTomy@users.noreply.github.com>
2024-04-14 17:51:25 -0600

    update backend for conversation history

--- fastapi_backend/main.py
+++ fastapi_backend/main.py
@@ -1,10 +1,11 @@
 import os
 from openai import AzureOpenAI
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.responses import RedirectResponse
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores.azuresearch import AzureSearch
 from pydantic import BaseModel
+from typing import List, Optional
 
 ################################################################
 # Environment variables
@@ -46,12 +47,41 @@ azure_search = AzureSearch(
 ###############################################################
 
 
+class ChatMessage(BaseModel):
+    role: str
+    content: str
+
+
 class Body(BaseModel):
     query: str
 
 
+class ChatHistory:
+    """Manages the history of a chat session, storing messages in a list
+    """
+    def __init__(self):
+        self.messages = []
+
+    def update(self, user_message: str, assistant_message: Optional[str] = None):
+        """Update the chat history with new messages from the user and
+        optionally from the assistant.
+
+        Args:
+            user_message (str): The message from the user to be added to the history
+            assistant_message (Optional[str], optional): The response from assistant to be added to the history. Defaults to None.
+        """
+        self.messages.append({"role": "user", "content": user_message})
+        if assistant_message:
+            self.messages.append({"role": "assistant", "content": assistant_message})
+
+    def get_history(self):
+        """Retrieves the entire chat history
+        """
+        return self.messages
+
+
 def search(query: str):
-    """Send the query to Azure Coginitive search and return
+    """Send the query to Azure AI search and return
     the top result
 
     Args:
@@ -70,28 +100,44 @@ def search(query: str):
     return result
 
 
-def assistant(query: str, context: str):
+def get_chat_history():
+    """Creates and returns a new instance of ChatHistory for managing chat messages.
+
+    Returns:
+        ChatHistory: A new instance of ChatHistory to track the messages of a chat session.
+    """
+    history = ChatHistory()
+    return history
+
+
+def assistant(query: str, context: str, chat_history: List[ChatMessage]):
     """Process user query and context to geenrate a response using Azure OpenAI API.
 
     Args:
         query (str): The input from the user to which the assistant will respond.
         context (str): Context retrieved from Azure AI Search that helps tailor the assistant's response.
+        chat_history(List[ChatMessage]): The current chat history that includes all messages so far.
 
     Returns:
-        str: The assistant's generated response to the user's query, incorporating the provided context.
+        str: The assistant's generated response to the user's query, incorporating the provided context and chat history.
     """
 
-    messages = [
-        {
-            "role": "system",
-            "content": "Assistant is called Barney and is a chatbot that helps you find the best wine for your taste.",
-        },
-        {
-            "role": "user",
-            "content": query,
-        },
-        {"role": "assistant", "content": context},
-    ]
+    messages = (
+        [
+            {
+                "role": "system",
+                "content": "Assistant is called Barney and is a chatbot that helps you find the best wine for your taste.",
+            }
+        ]
+        + chat_history
+        + [
+            {
+                "role": "user",
+                "content": query,
+            },
+            {"role": "assistant", "content": context},
+        ]
+    )
 
     client = AzureOpenAI(
         azure_endpoint=AZURE_OPENAI_ENDPOINT,
@@ -120,12 +166,18 @@ def root():
 
 
 @app.post("/ask")
-def ask(body: Body):
-    """Use the query parameter to interect with Azure OpenAI with
-    Azure Cognitive Search for Retreival Augmented Generation.
+def ask(body: Body, history: ChatHistory = Depends(get_chat_history)):
+    """Handles user queries by interfacing with Azure Open AI and Azure AI Search.
+
+    Args:
+        body (Body): The user's query encapusulated by Body object.
+        history (ChatHistory, optional): The chat history of the current session, managed by FastAPI's dependency injection. Defaults to Depends(get_chat_history).
+
+    Returns:
+        dict: A dictionary containing the 'response' key with the chatbot's generated text as its value.
     """
     search_result = search(body.query)
-    chat_bot_response = assistant(body.query, search_result)
+    chat_bot_response = assistant(body.query, search_result, history.get_history())
+    history.update(body.query, chat_bot_response)
 
     return {"response": chat_bot_response}
-
